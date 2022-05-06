e5ef7ec69f804dab3233ebaf799c1bb338eb5ba8
YangLi <61797788+lotuflo@users.noreply.github.com>
2022-05-06 16:23:39 +0800

    Update XSM_AutoOrder_2.0.js

--- XSM_AutoOrder_2.0.js
+++ XSM_AutoOrder_2.0.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         新商盟自动选购
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0
 // @description  try to take over the world!
 // @author       You
 // @match        http://gd.xinshangmeng.com:9090/eciop/orderForCC/*
