e9c983e2363740ff6a79942c961e465b78925f88
lotuflo <61797788+lotuflo@users.noreply.github.com>
2023-11-16 10:58:02 +0800

    fix conversation list bug

--- AUTOW.js
+++ AUTOW.js
@@ -1,24 +1,25 @@
 // ==UserScript==
-// @name         AUTOW
+// @name         AUTO
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  try to take over the world!
 // @author       You
 // @match        https://jinjianghotels.udesk.cn/entry/im
 // @match        https://jinjianghotels.udesk.cn/entry/analysis/im/record/index?end_date=&search=&start_date=&urlParams=
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=udesk.cn
 // @grant        none
+
 // ==/UserScript==
 /* globals $*/
 (function() {
   'use strict';
   //全局开关变量
   let lotuflo = true;
-
-  function tip() {
+  //span.robot-img-span___v57nc div
+  function tip(info) {
     // 创建提示元素
     var tooltip = document.createElement('div');
-    tooltip.textContent = '请检查';
+    tooltip.textContent = info;
     tooltip.style.position = 'fixed';
     tooltip.style.bottom = '50px';
     tooltip.style.right = '80px';
@@ -77,6 +78,7 @@
       movedFlag = true;
     }
   });
+  var isSimulatedClick = false;
   button.addEventListener('click', function() {
     //修改只剩开关功能
     lotuflo = !lotuflo;
@@ -92,7 +94,7 @@
   })
 
   //捕捉新建工单按钮而且必须唯一
-  async function execAuto() {
+  function execAuto() {
     if (!lotuflo) {
       lotuflo
       return;
@@ -102,16 +104,16 @@
       movedFlag = false;
       return;
     }
-
+    tip('ing');
     var count = 0;
-    while ($('span.udesk-webapp-ts-react-select-selection-item[title=请选择IT服务模板]').length != 1 && count < 10) {
+    while ($('span.udesk-webapp-ts-react-select-selection-item[title=请选择IT服务模板]').length != 1 && count < 6) {
       // 等待1秒钟
       setTimeout(function() {}, 1000);
       count++;
     }
     //判断是否存在请选择IT服务模板没有则不执行
     if ($('span.udesk-webapp-ts-react-select-selection-item[title=请选择IT服务模板]').length != 1) {
-      tip()
+      tip('请检查')
       return;
     }
     // 找框√
@@ -121,10 +123,11 @@
     if (masks.length > 0) {
       masks[masks.length - 1].dispatchEvent(evt)
     } else {
-      tip()
+      tip('请检查')
       return;
     }
     //这个X
+    event.stopPropagation();
     let str = 'div.udesk-webapp-ts-react-select-item.udesk-webapp-ts-react-select-item-option[title=';
     //先选择it模板
     $(str + 'IT服务模板]').click()
@@ -142,17 +145,46 @@
     $(str + '其它支持]').click();
     //选择问题类型
     $(str + '操作问题]').click();
+    isSimulatedClick = false;
   }
-  $(document).on('click', function(event) {
 
-    //点span或者button都会触发
-    var bnt1 = $($(event)[0].target).find('span').filter(function() {
-      return $(this).text().trim() == '新建工单';
-    }).length == 1;
-    var bnt2 = $($(event)[0].target).text() == '新建工单'
-    var bnt3 = $($(event)[0].target).closest('div.hight-light-icon-wrapper___3lRNO  ').find('span.t-create-ticket').length == 1
-    if (bnt1 || bnt2 || bnt3) {
-      setTimeout(execAuto, 4500);
+  $(document).on('click', function(event) {
+    // 解决循环调用问题
+    if (isSimulatedClick) {
+
+    isSimulatedClick = false;
+    return;
+  }
+    //怎么判断执行了第几次
+    var bnt1 = $($(event)[0].target).closest('div.hight-light-icon-wrapper___3lRNO  ').find('span.t-create-ticket').length == 1;
+    if (bnt1) {
+      setTimeout(execAuto, 6000);
+      return;
     }
+    var bnt2 = $(event)[0].target.textContent.includes('新建工单');
+    if (bnt2) {
+      setTimeout(execAuto, 6000);
+      return;
+    }
+    if ($('span.udesk-webapp-ts-react-select-selection-item[title=请选择IT服务模板]').length == 1) {
+      isSimulatedClick = true;
+      execAuto();
+      return;
+    }
+
+    //var bnt3 = $(event.target).is('div.header-section') && event.clientX == 0 && event.clientY == 0;
+    //由于event传导不正确因此,需要明确知道点击了的是新建按钮 而不是别的东西
+
+
+
+    /*if (bnt3) {
+      var bnt4 = $('div.udesk-webapp-react-row.udesk-webapp-react-row-center.my-12___2fZMz')[0];
+      if (bnt4) {
+        if (bnt4.textContent.includes('新建工单')) {
+          setTimeout(execAuto, 6000);
+          return;
+        }
+      }
+    }*/
   });
 })();
